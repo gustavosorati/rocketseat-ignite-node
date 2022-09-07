a297fca45127a9e72784ae6b8ab72f00f8d7c3c1
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-09-07 15:58:29 -0300

    chapter_bonus_backend_entregas M05 Aula 01 - Cadastro de entregas

---
+++ chapter_bonus-backend-de-entregas/src/modules/deliveries/useCases/createDelivery/CreateDeliveryUseCase.ts
@@ -0,0 +1,22 @@
+import { hash } from "bcrypt";
+import { response } from "express";
+import { prisma } from "../../../../database/prismaClient";
+
+interface ICreateDelivery {
+    item_name: string;
+    id_client: string;
+}
+
+
+export class CreateDeliveryUseCase {
+    async execute({item_name, id_client}: ICreateDelivery) {
+        const delivery = await prisma.deliveries.create({
+            data: {
+                item_name,
+                id_client
+            }
+        });
+
+        return delivery
+    }
+}
--- chapter_bonus-backend-de-entregas/src/routes.ts
+++ chapter_bonus-backend-de-entregas/src/routes.ts
@@ -2,7 +2,8 @@ import { Router } from "express";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { AuthenticateClientController } from "./modules/account/authenticateUser/AuthenticateClientController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
-import { CreateDeliverymanController } from "./modules/delivermans/useCase/createDeliveryman/CreateDeliverymanController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
+import { CreateDeliverymanController } from "./modules/deliverman/useCase/createDeliveryman/CreateDeliverymanController";
 
 
 const routes = Router();
@@ -13,11 +14,16 @@ const authenticateClientController = new AuthenticateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 
+const createDeliveryController = new CreateDeliveryController();
+
+
 routes.post("/client/", createClientController.handle);
 routes.post("/deliveryman/", createDeliverymanController.handle);
 
 routes.post("/client/authenticate/", authenticateClientController.handle);
 routes.post("/deliveryman/authenticate/", authenticateDeliverymanController.handle);
+
+routes.post("/delivery", createDeliveryController.handle);
 
 
 
