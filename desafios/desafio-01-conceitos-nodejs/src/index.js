af82aa4666456211b843f36946f357f3c8337c2d
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-12-08 20:00:25 -0300

    desafio-01-concluido

--- desafios/desafio-01-conceitos-nodejs/src/index.js
+++ desafios/desafio-01-conceitos-nodejs/src/index.js
@@ -12,6 +12,7 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   const { username } = request.headers;
+
   const userExists = users.find(user => user.username === username);
   if(!userExists) return response.status(404).json({  error: 'Usuário não encontrado'});
 
@@ -47,7 +48,6 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
   const {title, deadline } = request.body;
   const { user } = request;
 
-  const index = users.findIndex(obj => obj.username === user.username);
   
   const task = {
     id: uuidv4(),
@@ -58,7 +58,6 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
   }
 
   user.todos.push(task);
-  users[index] = user
 
   return response.status(201).json(task);
 });
@@ -68,47 +67,29 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   const {title, deadline} = request.body;
   const {id} = request.params;
 
-  const updateTodo = {
-    title,
-    deadline: new Date(deadline),
-    done: false
-  }
-
-  const todoIndex = user.todos.findIndex(todo => todo.id === id);
-  if(todoIndex < 0) return response.status(404).json({ error: 'Todo não encontrado'});
+  const todo = user.todos.find(todo => todo.id === id);
+  if(!todo) return response.status(404).json({ error: 'Todo não encontrado'});
   
-  user.todos[todoIndex] = {
-    ...user.todos[todoIndex],
-    ...updateTodo
-  }
+  todo.title = title,
+  todo.deadline = new Date(deadline)
 
   const index = users.findIndex(u => u.id === user.id);
   users[index] = user;
   
-  return response.json({ title, deadline, done: false});
+  return response.json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
   const {user} = request;
   const {id} = request.params;
 
-  const updateTodo = {
-    done: false
-  }
-
-  const todoIndex = user.todos.findIndex(todo => todo.id === id);
-  if(todoIndex < 0) return response.status(404).json({ error: 'Todo não encontrado'});
+  const todo = user.todos.find(todo => todo.id === id);
+  if(!todo) return response.status(404).json({ error: 'Todo não encontrado'});
   
-  user.todos[todoIndex] = {
-    ...user.todos[todoIndex],
-    done: true
-  }
+  todo.done = true;
 
-  const index = users.findIndex(u => u.id === user.id);
-  users[index] = user;
   
-  console.log(user.todos[todoIndex])
-  return response.json(user.todos[todoIndex]);
+  return response.json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
@@ -116,15 +97,10 @@ app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   const {id} = request.params;
 
   const todoIndex = user.todos.findIndex(todo => todo.id === id);
-  if(todoIndex < 0) return response.status(404).json({ error: 'Todo não encontrado'});
+  if(todoIndex === -1) return response.status(404).json({ error: 'Todo não encontrado'});
   
   user.todos.splice(todoIndex, 1);
 
-  const index = users.findIndex(u => u.id === user.id);
-  users[index] = user;
-
-
-  console.log(users)
   return response.status(204).json();
 });
 
