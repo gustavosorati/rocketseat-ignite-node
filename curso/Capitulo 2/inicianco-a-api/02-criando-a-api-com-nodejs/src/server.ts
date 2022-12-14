98ad84db526631c66c0cc6b171605e376e51d751
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-12-14 20:41:55 -0300

    aula-02 - Debugando a aplicação

--- curso/Capitulo 2/inicianco-a-api/02-criando-a-api-com-nodejs/src/server.ts
+++ curso/Capitulo 2/inicianco-a-api/02-criando-a-api-com-nodejs/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-import { createCourse } from './routes';
 
 const PORT = 3333;
 const app = express();
 
-app.get('/', createCourse);
+app.use(express.json());
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
+});
 
 app.listen(PORT, () => console.log('Server is running'));
