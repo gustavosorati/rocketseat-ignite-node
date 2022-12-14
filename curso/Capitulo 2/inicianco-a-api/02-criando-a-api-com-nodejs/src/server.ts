b7c6006206eed51d30e93e905a0abc5a812a1970
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-12-14 20:48:05 -0300

    aula-03 criando-categoria

--- curso/Capitulo 2/inicianco-a-api/02-criando-a-api-com-nodejs/src/server.ts
+++ curso/Capitulo 2/inicianco-a-api/02-criando-a-api-com-nodejs/src/server.ts
@@ -1,13 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const PORT = 3333;
 const app = express();
 
 app.use(express.json());
-
-app.post('/courses', (request, response) => {
-  const { name } = request.body;
-  return response.json({ name });
-});
+app.use(categoriesRoutes);
 
 app.listen(PORT, () => console.log('Server is running'));
