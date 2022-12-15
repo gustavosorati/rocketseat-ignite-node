d4bd748a9023decc75c412eb50a05efe5f72ebd7
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-12-15 18:20:23 -0300

    aula 02 - Criando repositório de especificação

--- curso/Capitulo 2/inicianco-a-api/04-continuacao-da-aplicacao/src/modules/cars/services/CreateSpecificationService.ts
+++ curso/Capitulo 2/inicianco-a-api/04-continuacao-da-aplicacao/src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,23 @@
+import { ISpecificationRepository } from '../repositories/ISpecificationRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
 class CreateSpecificationService {
-  execute() {
-    return;
+  constructor(private specificationRepository: ISpecificationRepository) {}
+
+  execute({ name, description }: IRequest) {
+    const specificationAlreadyExists =
+      this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExists)
+      throw new Error('Specification already exists!');
+
+    this.specificationRepository.create({
+      name,
+      description,
+    });
   }
 }
 
--- curso/Capitulo 2/inicianco-a-api/04-continuacao-da-aplicacao/src/server.ts
+++ curso/Capitulo 2/inicianco-a-api/04-continuacao-da-aplicacao/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationsRoutes } from './routes/specifications.routes';
 
 const PORT = 3333;
 const app = express();
 
 app.use(express.json());
 app.use('/categories', categoriesRoutes);
+app.use('/specifications', specificationsRoutes);
 
 app.listen(PORT, () => console.log('Server is running'));
