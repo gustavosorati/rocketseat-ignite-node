356f0da7cdc96d25fa33c635025a6d82cc2473d9
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-12-15 00:19:55 -0300

    aula 08 - Validando o cadastro de categoria

--- curso/Capitulo 2/inicianco-a-api/02-criando-a-api-com-nodejs/src/routes/categories.routes.ts
+++ curso/Capitulo 2/inicianco-a-api/02-criando-a-api-com-nodejs/src/routes/categories.routes.ts
@@ -7,6 +7,10 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
+  const categoriesAlreadyExists = categoriesRepository.findByName(name);
+  if (categoriesAlreadyExists)
+    return response.status(400).json({ error: 'Category alrady exists' });
+
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
