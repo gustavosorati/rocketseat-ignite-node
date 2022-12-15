58f10c6d8e708c0ef1bfbd58fe70c378c821bfde
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-12-15 19:16:17 -0300

    aula 03 - Criando os Use Case de Categoria

--- curso/Capitulo 2/inicianco-a-api/04-continuacao-da-aplicacao/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ curso/Capitulo 2/inicianco-a-api/04-continuacao-da-aplicacao/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,4 +1,4 @@
-import { ICategoriesRepository } from '../repositories/ICategoriesRepository';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
 interface IRequest {
   name: string;
@@ -11,7 +11,7 @@ interface IRequest {
  * [x] - Acessar o repositório
  */
 
-class CreateCategoryService {
+class CreateCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
   execute({ name, description }: IRequest) {
@@ -23,4 +23,4 @@ class CreateCategoryService {
   }
 }
 
-export { CreateCategoryService };
+export { CreateCategoryUseCase };
