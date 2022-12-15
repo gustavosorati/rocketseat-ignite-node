e836f8e8d1fb777b1c9a7c789c92338c08094515
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-12-14 21:57:11 -0300

    aula 06 - Criando repositório de categoria

--- curso/Capitulo 2/inicianco-a-api/02-criando-a-api-com-nodejs/src/routes/categories.routes.ts
+++ curso/Capitulo 2/inicianco-a-api/02-criando-a-api-com-nodejs/src/routes/categories.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { Category } from '../model/Category';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
