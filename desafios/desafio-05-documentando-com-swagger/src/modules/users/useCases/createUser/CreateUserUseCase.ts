3558183ce8f7d5abab13108f9f62343e40c71218
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-01-08 18:26:34 -0300

    desafio concluido

---
+++ desafios/desafio-05-documentando-com-swagger/src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,22 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ email, name }: IRequest): User {
+    const userExist = this.usersRepository.findByEmail(email);
+    if (userExist) throw new Error("This email is already in use.");
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
+  }
+}
+
+export { CreateUserUseCase };
---
+++ desafios/desafio-05-documentando-com-swagger/src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,21 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    const userAdmin = this.usersRepository.findById(user_id);
+    if (!userAdmin.admin)
+      throw new Error("You need to be an administrator to list all users");
+
+    const user = this.usersRepository.list();
+    return user;
+  }
+}
+
+export { ListAllUsersUseCase };
